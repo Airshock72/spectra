09242fc674f8e9bb20089fff819747b9818adb9f
Airshock72 <gelagela2222@gmail.com>
2024-07-16 00:07:54 +0400

    setup project with tailwind css and basic routes with configs

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import { Link } from 'expo-router'
+import {StatusBar} from 'expo-status-bar'
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-amber-200">
+            <Text className='text-3xl font-pblack'>Spectra!</Text>
+            <StatusBar style='auto' />
+            <Link style={{ color: 'blue' }} href='/profile'>Go to Profile</Link>
+        </View>
+    )
+}
