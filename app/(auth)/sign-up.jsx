67dd605a4e51753775fcb5864caa7b80ee1018a2
Airshock72 <gelagela2222@gmail.com>
2024-08-02 20:40:18 +0400

    configed appwrite and created user context

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Alert, Image, ScrollView, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -14,8 +15,19 @@ const SignUp = () => {
   })
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
-
+  const submit = async () => {
+    if (!form.email || !form.password || !form.username) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    setIsSubmitting(true)
+    try {
+      await createUser(form.email, form.password, form.username)
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
